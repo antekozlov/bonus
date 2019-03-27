d64a7e098073e3a76c0ab6effb393e6da64cd2ba
Anton Kozlov <ante.kozlov@gmail.com>
2019-03-27 17:25:39 +0300

    up

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const price = [200, 550, 4000, 23, 58, 5000, 485, 711];
-let sum = -10000;
+let sum = 0;
 for (const value of price) {
   sum += value;
 }
-const bonus = sum > 0 ? (sum * 5 / 100) : 0;
+const bonus = sum > 10000 ? (sum * 5 / 100) : 0;
 ceilBonus = Math.round(bonus);
 console.log(ceilBonus);
